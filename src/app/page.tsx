28d6c3bdee1b3fc443bd8bdb4f25e4f199213f71
Shreyas <1998chintoo@gmail.com>
2026-02-17 22:58:56 +0530

    final deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 import Contact from "./Contact/page";
 import Projects from "./projects/page";
 import Hero from "./Hero/page";
-import Reveal from "./Reveal/page";
-import Skills from "./Skills/page";
+import Reveal from "./reveal/page";
+import Skills from "./skills/page";
 
 const page = () => {
   return (
